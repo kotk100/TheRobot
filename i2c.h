4999882295d5da3d5dbe9ce05b851be02581f5bb
kotk100 <kern.ziga@gmail.com>
2015-08-15 16:04:19 +0200

    Initial

---
+++ i2c.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   i2c.h
+ * Author: Ziga
+ *
+ * Created on 15. avgust 2015, 9:13
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+
+#include <bcm2835.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+int test();
+
+#endif
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=therobot
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/therobot
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=therobot.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/therobot.tar
+# Release configuration
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_ARTIFACT_NAME_Release=therobot
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/therobot
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_PACKAGE_NAME_Release=therobot.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/therobot.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
