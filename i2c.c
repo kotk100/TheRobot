538c99394cdb1a26334b2dff5199340bba49a725
kotk100 <kern.ziga@gmail.com>
2015-08-16 23:46:12 +0200

    cleanup

--- i2c.c
+++ i2c.c
@@ -1,67 +1,3 @@
-/*******************************************************************************
-*
-*   i2c.c
-*
-*   Copyright (c) 2013 Shahrooz Shahparnia (sshahrooz@gmail.com)
-*
-*   Description:
-*   i2c is a command-line utility for executing i2c commands with the 
-*   Broadcom bcm2835.  It was developed and tested on a Raspberry Pi single-board
-*   computer model B.  The utility is based on the bcm2835 C library developed
-*   by Mike McCauley of Open System Consultants, http://www.open.com.au/mikem/bcm2835/.
-*
-*   Invoking spincl results in a read or write I2C transfer.  Options include the
-*   the I2C clock frequency, read/write, address, and port initialization/closing
-*   procedures.  The command usage and command-line parameters are described below
-*   in the showusage function, which prints the usage if no command-line parameters
-*   are included or if there are any command-line parameter errors.  Invoking i2c 
-*   requires root privilege.
-*
-*   This file contains the main function as well as functions for displaying
-*   usage and for parsing the command line.
-*
-*   Open Source Licensing GNU GPLv3
-*
-*   Building:
-* After installing bcm2835, you can build this 
-* with something like:
-* gcc -o i2c i2c.c -l bcm2835
-* sudo ./i2c
-*
-* Or you can test it before installing with:
-* gcc -o i2c -I ../../src ../../src/bcm2835.c i2c.c
-* sudo ./i2c
-*
-*   History:
-*   11/05    VERSION 1.0.0: Original
-*
-*      User input parsing (comparse) and showusage\
-*      have been adapted from: http://ipsolutionscorp.com/raspberry-pi-spi-utility/
-*      mostly to keep consistence with the spincl tool usage.
-*
-*      Compile with: gcc -o i2c i2c.c bcm2835.c
-*
-*      Examples:
-*
-*           Set up ADC (Arduino: ADC1015)
-*           sudo ./i2c -s72 -dw -ib 3 0x01 0x44 0x00 (select config register, setup mux, etc.)
-*           sudo ./i2c -s72 -dw -ib 1 0x00 (select ADC data register)
-*
-*           Bias DAC (Arduino: MCP4725) at some voltage
-*           sudo ./i2c -s99 -dw -ib 3 0x60 0x7F 0xF0 (FS output is with 0xFF 0xF0)
-*           Read ADC convergence result
-*           sudo ./i2c -s72 -dr -ib 2 (FS output is 0x7FF0 with PGA1 = 1)
-*  
-*      In a DAC to ADC loop back typical results are:
-*
-*      DAC    VOUT   ADC
-*      7FFh   1.6V   677h                    Note ratio is FS_ADC*PGA_GAIN/FS_DAC = 4.096/3.3 = 1.23
-*      5FFh   1.2V   4DCh
-*      8F0h   1.8V   745h
-*      9D0h   2V     7EAh
-*      000h   10mV   004h
-*
-********************************************************************************/
 #include "i2c.h"
 
 #include <bcm2835.h>
@@ -70,71 +6,18 @@
 #include <string.h>
 #include <stdint.h>
 
-#define MODE_READ 0
-#define MODE_WRITE 1
-#define MAX_LEN 32 //max lenght of len
-
-
-char wbuf[MAX_LEN] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-typedef enum {
-    NO_ACTION,
-    I2C_BEGIN,
-    I2C_END
-} i2c_init;
-uint8_t  init = I2C_BEGIN;
-uint16_t clk_div = BCM2835_I2C_CLOCK_DIVIDER_148;
-uint8_t slave_address = 0x32;
-uint32_t len = 8;
-uint8_t  mode = MODE_READ;
-
-/*
-int comparse(int argc, char **argv) {
-
-   
-    memset(wbuf, 0, sizeof(wbuf));
-    for (i = 0; i < xmitnum; i++) {
-        if (strspn(argv[argnum + i], "0123456789abcdefABCDEFxX") != strlen(argv[argnum + i])) {
-            fprintf(stderr, "Invalid data: ");
-            fprintf(stderr, "%d \n", xmitnum);
-            return EXIT_FAILURE;
-        }
-        wbuf[i] = (char)strtoul(argv[argnum + i], NULL, 0);
-    }
-    return EXIT_SUCCESS;
-}
-*/
-
 #define len 8
-#define addr 0x32
+#define SensorsAddr 0x32
 
 char data[len];
-
 
 void initI2CConnection(){
 	bcm2835_init();
 	bcm2835_i2c_begin();
-	bcm2835_i2c_setSlaveAddress(addr);
 }
-
 
 uint8_t * getSensorData() {
-		bcm2835_i2c_read(data, len);
-		return data;
+	bcm2835_i2c_setSlaveAddress(SensorsAddr);
+	bcm2835_i2c_read(data, len);
+	return data;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- i2c.h
+++ i2c.h
@@ -1,10 +1,3 @@
-/* 
- * File:   i2c.h
- * Author: Ziga
- *
- * Created on 15. avgust 2015, 9:13
- */
-
 #ifndef I2C_H
 #define	I2C_H
 
